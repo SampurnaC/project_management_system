b6514f636375b204e549adf4cda65e7038103122
SampurnaC <sampurna.chapagainnn@gmail.com>
2021-01-20 21:34:28 +0545

    uncomment comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,15 @@
-# class CommentsController < ApplicationController
-#   before_action :set_story, only: [:show, :edit, :update, :destroy]
+class CommentsController < ApplicationController
+  before_action :set_story, only: [:show, :edit, :update, :destroy]
 
-#   def create
-#     @story = Story.find(params[:story_id])
-#     @comment = @story.comments.create(params[:comment].permit(:comment_body, :user_id))
-#   end
+  def create
+    @story = Story.find(params[:story_id])
+    @comment = @story.comments.create(params[:comment].permit(:comment_body, :user_id))
+  end
 
 
 
-#   private
-#     def comment_params
-#       params.require(:story).permit(:comment_body, :user_id, :story_id)
-#     end
-# end
+  private
+    def comment_params
+      params.require(:story).permit(:comment_body, :user_id, :story_id)
+    end
+end
